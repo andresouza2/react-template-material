3012aa22fca94b4fc44a30874ecd778a5da85849
andre-souza <andre.souza@creathus.org.br>
2022-12-28 16:58:31 -0400

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@mui/material';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
+// import { Home } from "../pages";
+
+export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+  return (
+    <Routes>
+      <Route
+        path='/inicial'
+        element={
+          <Button variant='contained' color='primary' onClick={toggleTheme}>
+            Teste
+          </Button>
+        }
+      />
+
+      <Route path='/*' element={<Navigate to='/inicial' />} />
+    </Routes>
+  );
+};
